f8cfc883ac8137b988b238c4bc796b86ecba7187
Apostolos Samatas <flouts@gmail.com>
2013-09-28 16:26:58 +0200

    chapter 6 exercises

---
+++ chapter6/exercise6.1.rb
@@ -0,0 +1,3 @@
+puts "WHAT IS IT AGAIN MINION??"
+minions_request = gets.chomp
+puts 'WHADDAYA MEAN "' + minions_request.upcase + '"?!? YOU\'RE FIRED!!' 
---
+++ chapter6/exercise6.2.rb
@@ -0,0 +1,5 @@
+lw = 60
+puts 'Table of contents'.center(lw)
+puts 'Chapter 1:  ' + 'Getting Started'.ljust(lw/3) + 'page'.rjust(lw/3) + '1'.rjust(lw/20)
+puts 'Chapter 2:  ' + 'Numbers'.ljust(lw/3) + 'page'.rjust(lw/3) + '9'.rjust(lw/20)
+puts 'Chapter 3:  ' + 'Letters'.ljust(lw/3) + 'page'.rjust(lw/3) + '13'.rjust(lw/20)
