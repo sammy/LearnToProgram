eac92ffe00fdc2c63af601d68f78e0d925f1f476
Apostolos Samatas <flouts@gmail.com>
2013-09-28 18:08:05 +0200

    chapter 7 exersice 1

---
+++ chapter7/exersice7.1.rb
@@ -0,0 +1,18 @@
+bottles_of_beer = 5
+divider = '- '*32
+while bottles_of_beer != -1
+	if bottles_of_beer != 0
+	puts bottles_of_beer.to_s + ' bottles of beer on the wall, '+ bottles_of_beer.to_s + '  bottles of beer.'
+	puts 'Take one down, pass it around, ' + bottles_of_beer.to_s + ' bottles of beer on the wall...'
+	puts divider
+	bottles_of_beer = bottles_of_beer - 1
+	sleep(1)
+elsif bottles_of_beer == 0
+	puts 'No more bottles of beer on the wall, no more bottles of beer.'
+	puts 'Go to the store and buy some more'
+	break
+end
+end
+puts divider
+puts 'Had too much beer...cant walk to the store...i\'d rather go to the loo'
+puts divider
