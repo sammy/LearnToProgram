71e0bb432dbba4a314c89c59a222d0b59548ebaf
Apostolos Samatas <flouts@gmail.com>
2013-09-28 14:55:55 +0200

    chapter 1 excersises

---
+++ chapter1/excersise1.rb
@@ -0,0 +1,14 @@
+divider = "_________________________"
+puts divider
+puts "Hours in a year"
+puts 365*24
+puts divider
+puts "Minutes in a decade"
+puts ((365*10)*24)*60
+puts divider
+puts "My age in seconds"
+puts ((33*365)*24)*(60*60)
+puts divider
+puts "Authors Age"
+puts (((1160000000/60)/60)/24)/365
+puts divider
