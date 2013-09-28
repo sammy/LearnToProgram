6b44060cecee23eec5f7a32362ee3c97667355f9
Apostolos Samatas <flouts@gmail.com>
2013-09-28 15:51:27 +0200

    chapter 5 chapter 5 exercise

---
+++ chapter5/exercise5.1.rb
@@ -0,0 +1,8 @@
+puts "Hi there, what is your first name?"
+first_name = gets.chomp << ' '
+puts "..and what would be your middle name? If you dont have one hit enter"
+middle_name = gets.chomp << ' '
+puts "and finally what is your last name?"
+last_name = gets.chomp 
+
+puts 'My greetings '+ first_name +  middle_name + last_name + '!'
