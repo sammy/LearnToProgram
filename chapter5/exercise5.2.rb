bba46f48bb0cd21d3608190652b880e5ddbcaaef
Apostolos Samatas <flouts@gmail.com>
2013-09-28 15:58:38 +0200

    chapter 5 exersise 2

---
+++ chapter5/exercise5.2.rb
@@ -0,0 +1,4 @@
+puts "Hey! What is your favorite number?"
+fav_num = gets.chomp
+better_num = fav_num.to_i + 1
+puts "Have you ever considered " + better_num.to_s + " as a favorite number? Some people claim bigger is better!"
